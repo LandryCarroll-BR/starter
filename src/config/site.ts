1b6092254f6c166f4b192d38f916207d00f62230
Landry Carroll <lcarroll@fairwinds.org>
2024-03-23 18:06:53 -0400

    feat: shadcn-ui, prettier, eslint, fonts, config

---
+++ src/config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: 'Starter',
+  url: 'https://github.com/LandryCarroll-BR',
+  ogImage: 'https://github.com/LandryCarroll-BR/og.jpg',
+  description: 'My personal Next starter',
+  links: {
+    github: 'https://github.com/LandryCarroll-BR',
+  },
+}
+
+export type SiteConfig = typeof siteConfig
